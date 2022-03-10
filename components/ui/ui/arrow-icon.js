d673ed2c5eb02c4c243342fd2855f0a585ab943d
Edita Dani <editasefullaj@gmail.com>
2022-03-10 01:43:33 +0100

    add arrowIcon component

---
+++ components/ui/ui/arrow-icon.js
@@ -0,0 +1,22 @@
+const ArrowIcon = ({ classes, ...rest }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      role="img"
+      width="1.5em"
+      height="1.5em"
+      preserveAspectRatio="xMidYMid meet"
+      viewBox="0 0 24 24"
+      className={classes}
+      {...rest}
+    >
+      <path
+        fill="currentColor"
+        d="M11.67 3.87L9.9 2.1L0 12l9.9 9.9l1.77-1.77L3.54 12z"
+      />
+    </svg>
+  );
+};
+
+module.exports = ArrowIcon;
