8ad4a52164544a008d04bf2e3f54d46f54006b0b
Edita Dani <editasefullaj@gmail.com>
2022-03-10 01:44:01 +0100

    add offer page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import logo from '../public/images/logo.svg';
+
 import userIcon from '../public/icons/user.svg';
+import Logo from '../components/ui/Logo';
 
 export default function Home() {
   return (
     <>
       <header className="container relative w-full px-5 pt-8 pb-12 mx-auto overflow-hidden">
-        <div className="w-56">
-          <Image src={logo} alt="Binchonda logo" />
-        </div>
-
+        <Logo />
         <div className="absolute w-40 h-40 border-2 rounded-full shadow-md -top-10 -right-14">
           <div className="absolute flex flex-col items-center justify-center text-sm font-semibold text-gray-500 uppercase bottom-6 left-6">
             <Image src={userIcon} alt="user icon" height={36} width={36} />
@@ -20,8 +18,10 @@ export default function Home() {
           </div>
         </div>
       </header>
-      <main className="container px-5 mx-auto text-2xl text-gray-500">
-        <h1>Der Berliner Pflegedienst mit Durchblick und Expertise</h1>
+      <main className="container px-5 mx-auto">
+        <h1 className="text-2xl text-gray-500">
+          Der Berliner Pflegedienst mit Durchblick und Expertise
+        </h1>
 
         <div className="flex flex-col items-center mt-6">
           <Link href="/">
@@ -32,10 +32,10 @@ export default function Home() {
               Postleitzahel eingeben
             </a>
           </Link>
-          <Link href="/">
+          <Link href="/offers">
             <a
-              className="w-2/3 text-gray-100 bg-primary hover:bg-primary focus:ring-4 focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 my-2"
-              href="/"
+              className="w-2/3 text-gray-100 bg-primary hover:bg-fuchsia-800 focus:ring-4 focus:ring-fuchsia-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 my-2"
+              href="/offers"
             >
               Angebote ansehen
             </a>
---
+++ pages/offers.js
@@ -0,0 +1,121 @@
+import Logo from '../components/ui/Logo';
+import Link from 'next/link';
+import ArrowIcon from '../components/ui/ui/arrow-icon';
+
+export default function Home() {
+  return (
+    <>
+      <div className="container flex p-4 mx-auto">
+        <div className="flex flex-col items-center mr-8 text-gray-500">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            role="img"
+            width="1.5em"
+            height="1.5em"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M11.67 3.87L9.9 2.1L0 12l9.9 9.9l1.77-1.77L3.54 12z"
+            />
+          </svg>
+          <span>Zuruck</span>
+        </div>
+        <Logo />
+      </div>
+
+      <div className="container px-5 mx-auto mt-6">
+        <h1 className="text-2xl text-gray-500">Wir sind Fans von Spandau!</h1>
+        <p className="mt-2 text-gray-500">Tolle Nachrichten warten auf Sie:</p>
+        <div className="relative p-4 mt-6 bg-white rounded-lg shadow-md">
+          <div className="absolute flex items-center justify-center w-12 h-12 text-xs font-medium text-white rounded-full -top-4 -right-4 bg-primary">
+            NEU
+          </div>
+          <h3 className="font-semibold">Willkommenspacket im Dezember</h3>
+          <ul className="mt-5">
+            <li className="ml-6 before:absolute before:content-['\2014'] before:-ml-6">
+              Housreiningung
+            </li>
+          </ul>
+          <hr className="bottom-0 my-4 border-purple-300" />
+          <p className="mb-2">Im Wertvon €295,90</p>
+          <h5 className="font-medium">
+            Bie Pflegegrad 2 ubenimmt die Kasse 100% der Kosten.
+          </h5>
+          <div className="flex items-center justify-center mt-3">
+            <Link href="/offers">
+              <a
+                className="text-gray-100 bg-primary hover:bg-fuchsia-800 focus:ring-4 focus:ring-fuchsia-800 font-medium rounded-lg text-sm px-16 py-2.5 text-center mr-2 my-2"
+                href="/offers"
+              >
+                Unverbindlich Anfragen
+              </a>
+            </Link>
+          </div>
+          <p className="flex justify-center mt-3 text-xs text-primary">
+            Prufen ob Pflegezuschuss ouf mich zutrifft
+          </p>
+        </div>
+        <div>
+          <p className="mt-2 text-gray-500">Was wir in Spandau anbieten:</p>
+          <ul className="mt-3">
+            <li className="mt-3 text-gray-500 bg-white rounded-lg shadow-md hover:shadow-lg">
+              <Link href="/">
+                <a
+                  className="flex justify-between px-6 py-2 hover:text-gray-700"
+                  href="/"
+                >
+                  Fenster Reinigung <ArrowIcon classes="rotate-180" />
+                </a>
+              </Link>
+            </li>
+            <li className="mt-3 text-gray-500 bg-white rounded-lg shadow-md hover:shadow-lg">
+              <Link href="/">
+                <a
+                  className="flex justify-between px-6 py-2 hover:text-gray-700"
+                  href="/"
+                >
+                  Wohnungs Reinigung <ArrowIcon classes="rotate-180" />
+                </a>
+              </Link>
+            </li>
+            <li className="mt-3 text-gray-500 bg-white rounded-lg shadow-md hover:shadow-lg">
+              <Link href="/">
+                <a
+                  className="flex justify-between px-6 py-2 hover:text-gray-700"
+                  href="/"
+                >
+                  Garten Service
+                  <ArrowIcon classes="rotate-180" />
+                </a>
+              </Link>
+            </li>
+            <li className="mt-3 text-gray-500 bg-white rounded-lg shadow-md hover:shadow-lg">
+              <Link href="/">
+                <a
+                  className="flex justify-between px-6 py-2 hover:text-gray-700"
+                  href="/"
+                >
+                  Hausmeister Service <ArrowIcon classes="rotate-180" />
+                </a>
+              </Link>
+            </li>
+            <li className="mt-3 text-gray-500 bg-white rounded-lg shadow-md hover:shadow-lg">
+              <Link href="/">
+                <a
+                  className="flex justify-between px-6 py-2 hover:text-gray-700"
+                  href="/"
+                >
+                  Fahrdienst
+                  <ArrowIcon classes="rotate-180" />
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
